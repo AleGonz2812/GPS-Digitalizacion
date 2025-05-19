7ed6711940e1b2d21bef231c5a7d11825b0cbd48
titi2812 <agonvel.1307@gmail.com>
2025-05-19 02:24:05 +0200

    clase processor

--- TrabajoDig/src/GPSDataProcessor.java
+++ TrabajoDig/src/GPSDataProcessor.java
@@ -1,4 +1,37 @@
+import java.util.ArrayList;
+
 public class GPSDataProcessor {
 
+    /**
+     * Método que valida una lista de registros GPS y devuelve solo los que son válidos.
+     *
+     * @param datos Lista de objetos GPSData que han sido cargados desde el archivo CSV.
+     * @return Lista nueva que contiene solo los registros válidos.
+     */
+    public static ArrayList<GPSData> validarDatos(ArrayList<GPSData> datos) {
+        ArrayList<GPSData> validos = new ArrayList<>();
+        for (GPSData dato : datos) {
 
+            boolean valido = true;
+
+            if (dato.getLatitude() < -90 || dato.getLatitude() > 90) {
+                valido = false;
+            }
+            if (dato.getLongitude() < -180 || dato.getLongitude() > 180) {
+                valido = false;
+            }
+            if (dato.getSpeed() < 0 || dato.getSpeed() > 90) {
+                valido = false;
+            }
+            if (dato.getTimestamp() == null || dato.getTimestamp().isEmpty()) {
+                valido = false;
+            }
+            if (valido) {
+                validos.add(dato);
+            }
+
+        }
+        return validos;
+
+    }
 }
